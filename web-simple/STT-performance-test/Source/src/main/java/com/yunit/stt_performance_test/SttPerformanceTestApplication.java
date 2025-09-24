3879ead1a4ce6bab469750ec7d4fd0e95e94e046
yunit <yung9031@gmail.com>
2025-09-24 17:40:22 +0900

    new-project: STT performance test
    * STT 성능 테스트 페이지 개발
     - Gemini-CLI 활용
    
    * 프로젝트 기본 내용 정의
    
    * Spring boot 기본 소스코드 설정

---
+++ web-simple/STT-performance-test/Source/src/main/java/com/yunit/stt_performance_test/SttPerformanceTestApplication.java
@@ -0,0 +1,13 @@
+package com.yunit.stt_performance_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SttPerformanceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SttPerformanceTestApplication.class, args);
+	}
+
+}
---
+++ web-simple/STT-performance-test/Source/src/test/java/com/yunit/stt_performance_test/SttPerformanceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunit.stt_performance_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SttPerformanceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
