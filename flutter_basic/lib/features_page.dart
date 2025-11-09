5ff4a3d33d512684375c0328f477052bf6a3e0a9
yunit <yung9031@gmail.com>
2025-11-09 14:10:11 +0900

    feat: bottom navigation bar
    * 하단 내비게이션 바 추가
     - 홈 / 갤러리 / 기본기능 3개 페이지 추가
     - 기존 Flutter 기본 페이지는 기본 기능 페이지로 이동

---
+++ flutter_basic/lib/features_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class FeaturesPage extends StatefulWidget {
+  const FeaturesPage({super.key});
+
+  @override
+  State<FeaturesPage> createState() => _FeaturesPageState();
+}
+
+class _FeaturesPageState extends State<FeaturesPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ flutter_basic/lib/gallery_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GalleryPage extends StatelessWidget {
+  const GalleryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('갤러리 페이지'),
+    );
+  }
+}
